31887598de3300a80c64e03dee6a7d7141705dab
matsueT5374 <mishima001@users.noreply.github.com>
2018-01-16 11:45:07 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -610,6 +610,8 @@ $(function() {
           if (select_group_name == area_name) {
               window.alert("①○：" + areacount + "：" + select_group_name + "：" + area_name);
 
+              select_html += '<option value="-1">' + area_name + '</option>';
+
           } else {
               window.alert("②×：" + areacount + "：" + select_group_name + "：" + area_name);
 
@@ -620,11 +622,14 @@ $(function() {
 
               window.alert("③かうんと：" + areacount + "：" + select_group_name + "：" + area_name);
           }
+
+          var selected = (selected_name == area_name) ? 'selected="selected"': '';
+
+          select_html += '<option value="' + area_name + '" ' + selected + '>' + area_name + '</option>';
       }
 
-      var selected = (selected_name == area_name) ? 'selected="selected"': '';
 
-      select_html += '<option value="' + area_name + '" ' + selected + '>' + area_name + '</option>';
+
 
       areacount += 1;
     }
